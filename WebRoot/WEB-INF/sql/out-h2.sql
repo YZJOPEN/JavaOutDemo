85623972ed691d42761b4ae1c87fa6b2b6415aad
Gara <zhuo_ying@kingdee.com>
2018-08-17 10:35:57 +0800

    format sql

--- WebRoot/WEB-INF/sql/out-h2.sql
+++ WebRoot/WEB-INF/sql/out-h2.sql
@@ -1,1 +1,21 @@
-DROP TABLE IF EXISTS t_outrecord;CREATE TABLE t_outrecord (recordId varchar(125) NOT NULL,userName varchar(255) DEFAULT NULL,contact varchar(255) DEFAULT NULL,outTime varchar(125) NOT NULL,backTime varchar(125) NOT NULL ,outReason varchar(255) NOT NULL,photoUrl varchar(125) NOT NULL,deptName varchar(255) NOT NULL,state int(11) DEFAULT '1',orgId varchar(125) NOT NULL,openId varchar(125) NOT NULL,stage int(11),PRIMARY KEY (recordId));DROP TABLE IF EXISTS t_clockin;CREATE TABLE t_clockin (clockInId varchar(125) NOT NULL,position varchar(255) DEFAULT NULL,clockInTime varchar(125) NOT NULL,state int(11) DEFAULT '1',record_id varchar(125) DEFAULT NULL,PRIMARY KEY (clockInId));
+DROP TABLE IF EXISTS t_outrecord;
+CREATE TABLE t_outrecord (
+    recordId varchar(125) NOT NULL,
+    userName varchar(255) DEFAULT NULL,
+    contact varchar(255) DEFAULT NULL,
+    outTime varchar(125) NOT NULL,
+    backTime varchar(125) NOT NULL ,
+    outReason varchar(255) NOT NULL,
+    photoUrl varchar(125) NOT NULL,
+    deptName varchar(255) NOT NULL,
+    state int(11) DEFAULT '1',
+    orgId varchar(125) NOT NULL,
+    openId varchar(125) NOT NULL,
+    stage int(11),PRIMARY KEY (recordId));
+
+DROP TABLE IF EXISTS t_clockin;
+CREATE TABLE t_clockin (clockInId varchar(125) NOT NULL,
+position varchar(255) DEFAULT NULL,
+clockInTime varchar(125) NOT NULL,
+state int(11) DEFAULT '1',
+record_id varchar(125) DEFAULT NULL,PRIMARY KEY (clockInId));
